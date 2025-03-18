23a1b34af0a2464726f39164b40c4206fa471080
Juan Ruiz <rmjuanruiz@gmail.com>
2025-03-18 17:24:23 -0500

    feat: implement dark mode

--- projects/stage5/src/app/app.component.ts
+++ projects/stage5/src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core'
+import { Component, effect, signal } from '@angular/core'
 import { MatCardModule } from '@angular/material/card'
+import { MatIcon } from '@angular/material/icon'
+import { MatSlideToggle } from '@angular/material/slide-toggle'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { FlexModule } from '@ngbracket/ngx-layout/flex'
 
 import { CitySearchComponent } from './city-search/city-search.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
+
+const darkClassName = 'dark-theme'
+
 @Component({
   selector: 'app-root',
   template: `
     <div>
       <mat-toolbar color="primary">
         <span data-testid="title">LocalCast Weather</span>
+        <div fxFlex></div>
+        <mat-icon>brightness_5</mat-icon>
+        <mat-slide-toggle
+          color="warn"
+          data-testid="darkmode-toggle"
+          [checked]="toggleState()"
+          (change)="toggleState.set($event.checked)"></mat-slide-toggle>
+        <mat-icon>bedtime</mat-icon>
       </mat-toolbar>
       <div fxLayoutAlign="center">
         <div class="mat-caption v-pad">Your city, your forecast, right now!</div>
@@ -41,6 +54,17 @@ import { CurrentWeatherComponent } from './current-weather/current-weather.compo
     MatToolbarModule,
     MatCardModule,
     CitySearchComponent,
+    MatIcon,
+    MatSlideToggle,
   ],
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly toggleState = signal(localStorage.getItem('dark-theme') === 'true')
+
+  constructor() {
+    effect(() => {
+      localStorage.setItem(darkClassName, this.toggleState().toString())
+      document.documentElement.classList.toggle(darkClassName, this.toggleState())
+    })
+  }
+}
